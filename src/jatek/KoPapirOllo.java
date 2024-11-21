912d8354ae132891e23ca70ab65318f09adba192
fabuandras <fabuandras@gmail.com>
2024-11-21 15:35:27 +0100

    Ko-Papir-Ollo: felhasznaloValaszt & gepValaszt - kész, a felhasznalo & gep altal valasztott szamok szovegge konvertalasa - kesz

--- src/jatek/KoPapirOllo.java
+++ src/jatek/KoPapirOllo.java
@@ -1,20 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package jatek;
 
-/**
- *
- * @author FabuAndrásNorbert(SZ
- */
+import java.util.Random;
+import java.util.Scanner;
+
 public class KoPapirOllo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        Random rnd = new Random();
+        
+        System.out.println("Válassz: Kő (0), Papír (1), Olló (2)");
+        System.out.print("A te választásod: ");
+
+        int felhasznaloValasztas = sc.nextInt();
+        //...
+        int gepValasztas = rnd.nextInt(3);
+
+        String gepValasztasSzoveg;
+        String felhasznaloValasztasSzoveg;
+        
+        if (gepValasztas == 0) {
+            gepValasztasSzoveg = "Kő";
+        } else if (gepValasztas == 1) {
+            gepValasztasSzoveg = "Papír";
+        } else {
+            gepValasztasSzoveg = "Olló";
+        }
+
+        if (felhasznaloValasztas == 0) {
+            felhasznaloValasztasSzoveg = "Kő";
+        } else if (felhasznaloValasztas == 1) {
+            felhasznaloValasztasSzoveg = "Papír";
+        } else {
+            felhasznaloValasztasSzoveg = "Olló";
+        }
+        
+        System.out.println("A gép választása: " + gepValasztasSzoveg);
+        System.out.println("A te választásod: " + felhasznaloValasztasSzoveg);
     }
     
 }
+
+
+//        if (felhasznaloValasztas < 0 || felhasznaloValasztas > 2) {
+//            System.out.println("Érvénytelen választás! Csak 0, 1 vagy 2 lehet.");
+//            return;
+//        }
